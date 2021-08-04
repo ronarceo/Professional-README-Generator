ba797013f26b8aa81538cc1b7cd74a1f6818fa8c
ronarceo <ronaldmarceo@gmail.com>
2021-08-03 22:25:04 -0500

    added initial files
    made index.js
    installed inquirer package

---
+++ index.js
@@ -0,0 +1,58 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Provide a brief description of your project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions for your project?',
+            name: 'installation',
+        },
+        {
+            type: 'input' ,
+            message: 'Provide examples and instructions for use of your project.',
+            name: 'usage',
+        },
+        {
+            type: 'input' ,
+            message: 'List your collaborators or third party assets used if there are any.',
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: 'Provide tests for your project and how to run them.',
+            name: 'tests',
+        },
+        {
+            type: 'list' ,
+            message: 'Choose your license',
+            choices: [],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What is your Github username?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        }
+    ])
+    .then((response) => {
+        fs.writeFile('README.md',
+            ``, (err) =>
+            err ? console.error(err) : console.log('success'))
+    }
+    );
