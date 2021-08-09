6ce9eb99760e1850094ad567ce926f6de51f2c89
ronarceo <ronaldmarceo@gmail.com>
2021-08-08 20:00:29 -0500

    update to index.js
    added if statements to select corresponding license badge

--- index.js
+++ index.js
@@ -51,38 +51,62 @@ inquirer
         }
     ])
     .then((response) => {
-        fs.writeFile('./generated README/README.md',
-            `# ${response.title}
-            
-            ## Description
-            ${response.description}
+        let licenseBadge = '';
 
-            ## Table of Contents
-            - Installation
-            - Usage
-            - License
-            - Contributing
-            - Tests
-            - Questions
+        if (response.license == 'Apache 2.0 License') {
+            licenseBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        } else if (response.license == 'GNU GPL v3') {
+            licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        } else if (response.license == 'GNU GPL v2') {
+            licenseBadge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+        } else if (response.license == 'GNU AGPL v3') {
+            licenseBadge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+        } else if (response.license == 'GNU LGPL v3') {
+            licenseBadge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+        } else if (response.license == 'IBM Public License Version 1.0') {
+            licenseBadge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
+        } else if (response.license == 'ISC License (ISC)') {
+            licenseBadge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+        } else if (response.license == 'The MIT License') {
+            licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        } else if (response.license == 'Mozilla Public License 2.0') {
+            licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+        }
+
+        fs.writeFile('./generated README/README.md',
+            `${licenseBadge}
+
+# ${response.title}
             
-            ## Installation
-            ${response.installation}
+## Description
+${response.description}
+
+## Table of Contents
+- Installation
+- Usage
+- License
+- Contributing
+- Tests
+- Questions
             
-            ## Usage
-            ${response.usage}
+## Installation
+${response.installation}
             
-            ## License
-            ${response.license}
+## Usage
+${response.usage}
             
-            ## Contributing
-            ${response.contributions}
+## License
+${response.license}
             
-            ## Tests
-            ${response.tests}
+## Contributing
+${response.contributions}
             
-            ## Questions
-            Github: github.com/${response.username}
-            Email: ${response.email}`, (err) =>
-            err ? console.error(err) : console.log('success'))
+## Tests
+${response.tests}
+            
+## Questions
+Github: github.com/${response.username}
+Email: ${response.email}`, (err) =>
+err ? console.error(err) : console.log('README successfuly generated'))
     }
     );
